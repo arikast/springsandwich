390e069395efadfabcc70259f47bb939d742a0fd
ari <ari@mascii.org>
2017-04-26 06:30:18 -0400

    restore nop super calls in case they ever acquire needed side effects in future

--- src/main/java/com/kastkode/springsandwich/filter/internal/InterceptDelegator.java
+++ src/main/java/com/kastkode/springsandwich/filter/internal/InterceptDelegator.java
@@ -32,7 +32,7 @@ public class InterceptDelegator extends HandlerInterceptorAdapter {
         if (!(handler instanceof HandlerMethod)) return true;
 
         HandlerMethod handlerMethod = (HandlerMethod) handler;
-	      Before classInterceptors = handlerMethod.getMethod().getDeclaringClass().getAnnotation(Before.class);
+        Before classInterceptors = handlerMethod.getMethod().getDeclaringClass().getAnnotation(Before.class);
         if (! preHandleInterceptors(classInterceptors, request, response, handlerMethod)) {
         	return false;
         }
@@ -42,7 +42,8 @@ public class InterceptDelegator extends HandlerInterceptorAdapter {
         	return false;
         }
 
-        return true;
+        //currently just returns true, but who knows what necessary side-effects may be added in future
+        return super.preHandle(request, response, handler);
     }
 
 	@Override
@@ -50,35 +51,38 @@ public class InterceptDelegator extends HandlerInterceptorAdapter {
         if (!(handler instanceof HandlerMethod)) return;
 
         HandlerMethod handlerMethod = (HandlerMethod) handler;
-	      After classInterceptors = handlerMethod.getMethod().getDeclaringClass().getAnnotation(After.class);
+	    After classInterceptors = handlerMethod.getMethod().getDeclaringClass().getAnnotation(After.class);
         postHandleInterceptors(classInterceptors, request, response, handlerMethod, modelAndView);
 
-	      After methodInterceptors = handlerMethod.getMethod().getAnnotation(After.class);
+	    After methodInterceptors = handlerMethod.getMethod().getAnnotation(After.class);
         postHandleInterceptors(methodInterceptors, request, response, handlerMethod, modelAndView);
+        
+        //currently a nop, but who knows what required side-effects may be added in future
+        super.postHandle(request, response, handler, modelAndView);
 	}
 
 	private boolean preHandleInterceptors(Before interceptors, HttpServletRequest request, HttpServletResponse response,
-			HandlerMethod handlerMethod) throws Exception {
-		  if(interceptors == null) return true;
+		HandlerMethod handlerMethod) throws Exception {
+		if(interceptors == null) return true;
 
-		  Flow result = Flow.CONTINUE;
-		  for(BeforeElement classWithArgs:interceptors.value()) {
-    	  	BeforeHandler interceptInstance = appContext.getBean(classWithArgs.value());
-    		  result = interceptInstance.handle(request, response, handlerMethod, classWithArgs.flags());
-    		  if(result != Flow.CONTINUE) break;
+		Flow result = Flow.CONTINUE;
+		for(BeforeElement classWithArgs:interceptors.value()) {
+			BeforeHandler interceptInstance = appContext.getBean(classWithArgs.value());
+			result = interceptInstance.handle(request, response, handlerMethod, classWithArgs.flags());
+			if(result != Flow.CONTINUE) break;
     	}
-		  return (result == Flow.CONTINUE);
+		return (result == Flow.CONTINUE);
 	}
 
 	private void postHandleInterceptors(After interceptors, HttpServletRequest request, HttpServletResponse response,
-			HandlerMethod handlerMethod, ModelAndView modelAndView) throws Exception {
-			if(interceptors == null) return;
+		HandlerMethod handlerMethod, ModelAndView modelAndView) throws Exception {
+		if(interceptors == null) return;
 
-			Flow result = Flow.CONTINUE;
-			for(AfterElement classWithArgs:interceptors.value()) {
-	    		AfterHandler interceptInstance = appContext.getBean(classWithArgs.value());
-	    		interceptInstance.handle(request, response, handlerMethod, modelAndView, classWithArgs.flags());
-	    		if(result != Flow.CONTINUE) break;
-	    	}
-		}
+		Flow result = Flow.CONTINUE;
+		for(AfterElement classWithArgs:interceptors.value()) {
+	    	AfterHandler interceptInstance = appContext.getBean(classWithArgs.value());
+	    	interceptInstance.handle(request, response, handlerMethod, modelAndView, classWithArgs.flags());
+	    	if(result != Flow.CONTINUE) break;
+	    }
+	}
 }
